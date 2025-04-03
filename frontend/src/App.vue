2e300139e982595511578ec117a6e121e35a069e
Klezovich <olegolegolegoleg88@gmail.com>
2025-04-04 00:40:21 +0300

    frontend: fixed global font-family

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+// import "@fontsource/commissioner"
+import '@fontsource/commissioner/400.css'
+import '@fontsource/commissioner/500.css'
+import '@fontsource/commissioner/600.css'
+import '@fontsource/nunito'
+import '@fontsource/nunito/700.css'
 </script>
 
 <template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,8 +5,9 @@ import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import router from '@/utils/router';
 import logger from '@/plugins/logger';
-import '@/scss/styles.scss';
-import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@/assets/styles.scss';
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
