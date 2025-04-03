4ff4099d107a6c977eab7d92f89a862c7b89d308
Klezovich <olegolegolegoleg88@gmail.com>
2025-04-04 00:53:57 +0300

    frontend: started work with error/403 page

---
+++ frontend/src/pages/error/403.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+ 
+ <template>
+     <div>Не авторизован.</div>
+ </template>
+ 
+ <style lang="postcss" scoped></style>
--- frontend/src/pages/test/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>hello from test</div>
-</template>
