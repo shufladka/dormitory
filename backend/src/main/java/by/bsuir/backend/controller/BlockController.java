18fd20c533a96260826792fcbbe2f449046cec7c
Klezovich <olegolegolegoleg88@gmail.com>
2025-04-07 01:15:24 +0300

    backend: removed unused pageable func

--- backend/src/main/java/by/bsuir/backend/controller/BlockController.java
+++ backend/src/main/java/by/bsuir/backend/controller/BlockController.java
@@ -28,14 +28,8 @@ public class BlockController extends AbstractController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<BlockResponseTo> findAll(@RequestParam(defaultValue = "0") Integer pageNumber,
-                                        @RequestParam(defaultValue = "5") Integer pageSize,
-                                        @RequestParam(defaultValue = "id,desc") String[] sortParameters) {
-
-        List<Sort.Order> sortOrders = getSortOrderList(sortParameters);
-        Pageable restriction = PageRequest.of(pageNumber, pageSize, Sort.by(sortOrders));
-
-        return service.findAll(restriction);
+    public List<BlockResponseTo> findAll() {
+        return service.findAll(Pageable.unpaged());
     }
 
     @GetMapping("/dormitory/{id}")
--- backend/src/main/java/by/bsuir/backend/controller/ResidentController.java
+++ backend/src/main/java/by/bsuir/backend/controller/ResidentController.java
@@ -28,14 +28,8 @@ public class ResidentController extends AbstractController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ResidentResponseTo> findAll(@RequestParam(defaultValue = "0") Integer pageNumber,
-                                        @RequestParam(defaultValue = "5") Integer pageSize,
-                                        @RequestParam(defaultValue = "id,desc") String[] sortParameters) {
-
-        List<Sort.Order> sortOrders = getSortOrderList(sortParameters);
-        Pageable restriction = PageRequest.of(pageNumber, pageSize, Sort.by(sortOrders));
-
-        return service.findAll(restriction);
+    public List<ResidentResponseTo> findAll() {
+        return service.findAll(Pageable.unpaged());
     }
 
     @GetMapping("/{id}")
